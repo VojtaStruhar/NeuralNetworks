c7d95d19d42310dbf18f4bc7c52dc4b7b20db052
Vojtěch Struhár <vojtechstruhar@gmail.com>
2023-11-08 19:29:39 +0100

    Adjusted run.sh script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd NeuralNetworkProject
+
+dotnet run
+
+## use nice to decrease priority in order to comply with aisa rules
+## https://www.fi.muni.cz/tech/unix/computation.html.en
+## especially if you are using multiple cores
+# nice -n 19 ./network
